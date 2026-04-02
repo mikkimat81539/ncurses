5806fd1872f54fd8caf2e39e3c3c100b7466e873
mikkimat81539 <mmatthews81539@gmail.com>
2026-04-02 23:39:00 +0000

    created conditional statments for player actions

--- testEnums.cpp
+++ testEnums.cpp
@@ -4,21 +4,45 @@ using namespace std;
 
 int main(){
 	int energy = 100;
-	int selection;
+	int userSelection;
+	int deplete;
 
 	enum Actions {
-		STAND = 0,
-		WALK = 25,
-		SKIP = 50,
-		JOG = 75,
-		RUN = 100
+		STAND,
+		WALK,
+		SKIP,
+		JOG,
+		RUN
 	};	
 
-	cout << "current energy at " << energy << " percent." << endl;	
+	cout << "Current energy at " << energy << " percent." << endl;	
 
-	cout << "Do you want to STAND, WALK, SKIP, JOG, OR RUN: ";
+	cout << "Do you want to STAND(0), WALK(1), SKIP(2), JOG(3), OR RUN(4): ";
 
-	cin >> selection;
+	cin >> userSelection;
 
+	enum Actions stand = STAND;
+	enum Actions walk = WALK;
+	enum Actions skip = SKIP;
+	enum Actions jog = JOG;
+	enum Actions run = RUN;
+
+	if(userSelection  == stand){
+		cout << "You have used up 0 percent energy" << endl;
+		cout << "Your current energy is " << energy << " percent" << endl;
+	}	
+
+	else if(userSelection == walk){
+		deplete = energy - 25;
+		cout << "You have used up 25 percent energy" << endl;
+		cout << "You have " << deplete << " percent remaining" << endl; 	
+	}
+
+	else if(userSelection == skip){};
+
+	else if (userSelection == jog){};
+
+	else if (userSelection == run){};
+	
 	return 0;
 }
