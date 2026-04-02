5825d7551840848377a16615f2fd0ded19b92a87
mikkimat81539 <mmatthews81539@gmail.com>
2026-04-02 22:08:11 +0000

    displayed current energy levels

--- testEnums.cpp
+++ testEnums.cpp
@@ -1,9 +1,19 @@
-#include <cstdio>
+#include <iostream>
 
 using namespace std;
 
 int main(){
-	printf("Hello World\n");
+	int energy = 100;
+
+	enum Actions {
+		STAND = 0,
+		WALK = 25,
+		SKIP = 50,
+		JOG = 75,
+		RUN = 100
+	};	
+
+	cout << "current energy at " << energy << " percent." << endl;	
 
 	return 0;
 }
