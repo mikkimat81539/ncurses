eaa8095474dee0e6325603e7d416f1d07a5d5225
mikkimat81539 <mmatthews81539@gmail.com>
2026-04-03 02:35:41 +0000

    created conditional statements for all player actions

--- testEnums.cpp
+++ testEnums.cpp
@@ -35,14 +35,26 @@ int main(){
 	else if(userSelection == walk){
 		deplete = energy - 25;
 		cout << "You have used up 25 percent energy" << endl;
-		cout << "You have " << deplete << " percent remaining" << endl; 	
+		cout << "You have " << deplete << " percent energy remaining" << endl; 	
 	}
 
-	else if(userSelection == skip){};
+	else if(userSelection == skip){
+		deplete = energy - 50;
+		cout << "You have used 50 percent energy" << endl;
+		cout << "You have " << deplete << " percent energy remaining" << endl;
+	}
 
-	else if (userSelection == jog){};
+	else if(userSelection == jog){
+		deplete = energy - 75;
+		cout << "You have used 75 percent energy" << endl;
+		cout << "You have " << deplete << " percent energy remaining" << endl;
+	}
 
-	else if (userSelection == run){};
+	else if(userSelection == run){
+		deplete = energy - 100;
+		cout << "You have used 100 percent energy" << endl;
+		cout << "You have " << deplete << " percent energy remaining" << endl;
+	}
 	
 	return 0;
 }
