4a7e31121a1bc3ebae51ea8ea962a6659796abfb
mikkimat81539 <mmatthews81539@gmail.com>
2026-04-05 07:17:49 +0000

    created context class for state pattern

---
+++ state1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+enum controlSwitch { // List out the states
+	ON = 1,
+	OFF = 0
+};
+
+class Light { // Context class
+	public:
+	enum controlSwitch bulb; // create an enum variable
+
+	Light(controlSwitch state){ // Constructor
+		bulb = state; // enum variable = state parameter
+	}
+};
+
+int main(){
+	Light myLight(OFF); // create class variable
+	
+	cout << myLight.bulb << endl; // reads the value of the enum
+	
+	return 0;
+}
